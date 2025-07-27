83c2b623697870ab88f30016dce931c4783051db
arapoport918 <arapoport918@gmail.com>
2025-07-27 16:07:31 -0400

    Add basic routes

--- greased-poles/src/app/app.routes.ts
+++ greased-poles/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './features/homepage/homepage.component';
+import { LeaderboardComponent } from './features/leaderboard/leaderboard.component';
+import { PlayerComponent } from './features/player/player.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomepageComponent},
+    {path: 'leaderboard', component: LeaderboardComponent},
+    {path: 'player/:id', component: PlayerComponent},
+];
